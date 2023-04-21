2be9e8bf7e053f6a57513f2422f5e5092b99a488
justyna <towarnickaj@gmail.com>
2023-04-21 13:46:42 +0200

    alias

--- network/skrypt.sh
+++ network/skrypt.sh
@@ -2,21 +2,21 @@
 
 # You can use docker compose or docker commands to solve this exercise :)
 
-docker compose build
-docker compose up
+# docker compose build
+# docker compose up
 
-# # create networks
-# docker network create --driver bridge backend
-# docker network create --driver bridge frontend
+# create networks
+docker network create --driver bridge backend
+docker network create --driver bridge frontend
 
-# # build images
-# docker build -t database ./database
-# docker build -t backend ./backend
-# docker build -t frontend ./frontend
+# build images
+docker build -t database ./database
+docker build -t backend ./backend
+docker build -t frontend ./frontend
 
-# # run containers and connect them to networks
-# docker run -d --name backend --network backend backend
-# docker run -d --name database --network backend --link database:database database
-# docker network connect frontend backend
-# docker run -d --name frontend --network frontend -p 8080:80 --link backend:backend frontend
+# run containers and connect them to networks
+docker run -d --name backend --network backend backend
+docker run -d --name database --network backend --network-alias database database
+docker network connect frontend backend
+docker run -d --name frontend --network frontend -p 8080:80 --network-alias backend frontend
 
